69c7678cc77eeb5e8fb5bdea60e02efa1ca1a67c
AdSinghh <132607841+AdSinghh@users.noreply.github.com>
2024-08-27 18:16:39 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,51 @@
 const url = "https://www.espncricinfo.com/series/ipl-2020-21-1210595";
-// Venue date opponent result runs balls fours sixes sr
+
 const request = require("request");
 const cheerio = require("cheerio");
-const AllMatcgObj = require("./Allmatch");
-// home page 
+
+const allMatchObj = require('./AllMatch')
+
+const path = require('path')
+
+const fs= require('fs')
+
+
+let iplPath = path.join(__dirname , "IPL")
+
+console.log(__dirname)
+
+dirCreator(iplPath)
+
+
+
 request(url, cb);
-function cb(err, response, html) {
-    if (err) {
-        console.log(err);
-    } else {
-        // console.log(html);
-        extractLink(html);
-    }
+
+function cb(error, response, html) {
+  if (error) {
+    console.log(error);
+  } else {
+    extractLink(html);
+  }
 }
+
 function extractLink(html) {
-    let $ = cheerio.load(html);
-    let anchorElem = $("a[data-hover='View All Results']");
-    let link = anchorElem.attr("href");
-    // console.log(link);
-    let fullLink = "https://www.espncricinfo.com" + link;
-    // console.log(fullLink);
-    AllMatcgObj.gAlmatches(fullLink);
+  let $ = cheerio.load(html);
+
+  let anchorElem = $('a[data-hover="View All Results"]');
+
+  let link = anchorElem.attr("href");
+
+  let fullLink = "https://www.espncricinfo.com" + link;
+
+  console.log(fullLink);
+
+  allMatchObj.getAllMatch(fullLink)
+}
+
+
+function dirCreator(filePath){
+  if(fs.existsSync(filePath)==false){
+    fs.mkdirSync(filePath)
+  }
+}
 }
